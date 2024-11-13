6b7e3c42e487344ab6b6ae374d4867e62f914ca3
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-11-13 22:18:41 +0200

    added images, needs some final css

--- components/unauthorized/landing-page/landing-page-sections/SecondSection.component.tsx
+++ components/unauthorized/landing-page/landing-page-sections/SecondSection.component.tsx
@@ -2,23 +2,28 @@ import Image from "next/image";
 
 export default function SecondSection() {
   const imageURLs = [
-    "@/public/images/pngwing_pepsi.png",
-    "@/public/images/pngwingAdidas.png",
-    "@/public/images/pngwingBarbie.png",
-    "@/public/images/pngwingBurberry.png",
-    "@/public/images/pngwingCalvinKlein.png",
-    "@/public/images/pngwingPuma.png",
-    "@/public/images/pngwingZara.png",
+    { url: "/images/pngwing_pepsi.png", id: 1 },
+    { url: "/images/pngwingAdidas.png", id: 2 },
+    { url: "/images/pngwingBarbie.png", id: 3 },
+    { url: "/images/pngwingBurberry.png", id: 4 },
+    { url: "/images/pngwingCalvinKlein.png", id: 5 },
+    { url: "/images/pngwingPuma.png", id: 6 },
+    { url: "/images/pngwingZara.png", id: 7 },
   ];
 
   return (
-    <div>
-      <div>
-        {imageURLs.map((url) => {
+    <div className="flex-row border-2 border-red-900">
+      <div className="margin-auto w-full items-center justify-around flex flex-row border-2">
+        {imageURLs.map((imageObject) => {
           return (
-            <>
-              <Image src={url} alt={`someImage`} width={150} height={100} />
-            </>
+            <Image
+              key={imageObject.id}
+              src={imageObject.url}
+              alt={`someImage`}
+              width={150}
+              height={150}
+              className="w-50 h-auto filter grayscale "
+            />
           );
         })}
       </div>
