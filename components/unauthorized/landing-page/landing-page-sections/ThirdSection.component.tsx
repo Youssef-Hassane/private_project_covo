743d7dfe132cdb3b31cce22c94ae3ad122380e2b
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-11-13 23:06:19 +0200

    new branch ThirdSection initialized for about section

--- components/unauthorized/landing-page/landing-page-sections/ThirdSection.component.tsx
+++ components/unauthorized/landing-page/landing-page-sections/ThirdSection.component.tsx
@@ -1,8 +1,8 @@
-
+import Image from 'next/image';
 
 export default function ThirdSection() {
   return (
-    <div>
+    <div className="flex flex-row">
       <h1>Section 3</h1>
       <p>Section 3 content</p>
     </div>
