3cc8b2cb209ff6df606692f5074fc8cd6ced830e
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-11-13 21:16:37 +0200

    initial structure for secondSection component, not tested on main page. removed test.txt

--- components/unauthorized/landing-page/landing-page-sections/SecondSection.component.tsx
+++ components/unauthorized/landing-page/landing-page-sections/SecondSection.component.tsx
@@ -1,11 +1,27 @@
-import Image from 'next/image';
-
+import Image from "next/image";
 
 export default function SecondSection() {
+  const imageURLs = [
+    "@/public/images/pngwing_pepsi.png",
+    "@/public/images/pngwingAdidas.png",
+    "@/public/images/pngwingBarbie.png",
+    "@/public/images/pngwingBurberry.png",
+    "@/public/images/pngwingCalvinKlein.png",
+    "@/public/images/pngwingPuma.png",
+    "@/public/images/pngwingZara.png",
+  ];
+
   return (
     <div>
-      <h1>Section 2</h1>
-      <p>Section 2 content</p>
+      <div>
+        {imageURLs.map((url) => {
+          return (
+            <>
+              <Image src={url} alt={`someImage`} width={150} height={100} />
+            </>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
