1db5c7ab2d570d1fc1b219608cb308936351ae98
youssef-hassane <youssef_hassane@icloud.com>
2024-11-13 18:49:37 +0200

    adding the landing page struc/files

---
+++ app/(unauthorized)/landingpage/page.tsx
@@ -0,0 +1,12 @@
+
+export default function LandingPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Landing Page</h1>
+    </main>
+  );
+
+  // return (
+  //   <LandingPage />
+  // );
+}
