36a95d8355b7b9543c86a28ed757e3f9e14503a2
youssef-hassane <youssef_hassane@icloud.com>
2024-11-14 19:51:24 +0200

    Fourth Section - Not DONE Yet

--- components/unauthorized/landing-page/landing-page-sections/FourthSection.component.tsx
+++ components/unauthorized/landing-page/landing-page-sections/FourthSection.component.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export default function FourthSection() {
   const cardInformation = [
-    { url: "/images/pngwing_pepsi.png", text: "Discover Influencers", supText: "Our platform offers a user-friendly interface for easily searching and filtering influencers based on various criteria, including niche, audience demographics, engagement rates and location.", id: 1 },
-    { url: "/images/pngwing_pepsi.png", text: "Build Your Campaign", supText: "Our platform provides a streamlined process for creating and customizing campaigns, allowing brands to define their objectives, target audience, budget, and desired deliverables.", id: 2 },
-    { url: "/images/pngwing_pepsi.png", text: "Cloud-Enabled", supText: "Our cloud-based platform ensures scalability, accommodating both small and large-scale campaigns without compromising performance.", id: 3 },
-    { url: "/images/pngwing_pepsi.png", text: "Manage Ongoing Campaigns", supText: "Our cloud-based platform ensures scalability, accommodating both small and large-scale campaigns without compromising performance.", id: 3 },
-    { url: "/images/pngwing_pepsi.png", text: "Cloud-Enabled", supText: "Our cloud-based platform ensures scalability, accommodating both small and large-scale campaigns without compromising performance.", id: 3 },
-    { url: "/images/pngwing_pepsi.png", text: "Cloud-Enabled", supText: "Our cloud-based platform ensures scalability, accommodating both small and large-scale campaigns without compromising performance.", id: 3 },
+    { url: "/svg/people_search.svg", text: "Discover Influencers", supText: "Our platform offers a user-friendly interface for easily searching and filtering influencers based on various criteria, including niche, audience demographics, engagement rates and location.", id: 1 },
+    { url: "/svg/building_blocks.svg", text: "Build Your Campaign", supText: "Our platform provides a streamlined process for creating and customizing campaigns, allowing brands to define their objectives, target audience, budget, and desired deliverables.", id: 2 },
+    { url: "/svg/software_engineer.svg", text: "Cloud-Enabled", supText: "Our cloud-based platform ensures scalability, accommodating both small and large-scale campaigns without compromising performance.", id: 3 },
+    { url: "/svg/co_workers.svg", text: "Manage Ongoing Campaigns", supText: "Stay informed about the progress of your campaigns through real-time updates and notifications.", id: 4 },
+    { url: "/svg/chat_bot.svg", text: "AI-Driven Recommendations", supText: "Leveraging AI-powered algorithms, we provide precise matches between brands and influencers, ensuring optimal campaign alignment.", id: 5 },
+    { url: "/svg/secure_login.svg", text: "Encryption and Security", supText: "Rest assured that your data is safeguarded with robust encryption protocols, ensuring the confidentiality and integrity of your information.", id: 6 },
 
 
   ];
@@ -22,15 +22,15 @@ export default function FourthSection() {
         <p className="text-1xl font-[600] text-custom-dark-desaturated-blue text-center">Simplify your influencer marketing with our all-in-one platform, offering cost-effective solutions while streamlining your influencer marketing efforts and maximize ROI with our affordable platform.</p>
       </div>
       <br />
-      <div className="flex gap-2 justify-center items-center">
+      <div className="flex  gap-2 justify-center items-center">
         {cardInformation.map((cardObject) => (
           <div key={cardObject.id} className="bg-white w-[400px] h-[400px] rounded-lg border-custom-light-grayish-blue border-2 flex flex-col justify-center items-center p-4">
             <Image
               src={cardObject.url}
               alt={`Brand logo ${cardObject.id}`}
-              width={150}
-              height={150}
-              className="w-[100px] h-auto filter grayscale transition-opacity hover:grayscale-0"
+              width={200}
+              height={200}
+              className="w-[100px] h-auto "
             />
             <br />
             <p className="text-2xl font-[600] text-custom-dark-desaturated-blue text-center">{cardObject.text}</p>
