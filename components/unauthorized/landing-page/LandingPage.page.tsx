1b3d9943aad1ccf4f11033955e0123ba592b11e5
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-11-25 22:32:16 +0200

    changing file structure by adding layout.tsx in the root of (unauthorized) and adding to it the header and footer

--- components/unauthorized/landing-page/LandingPage.page.tsx
+++ components/unauthorized/landing-page/LandingPage.page.tsx
@@ -5,13 +5,10 @@ import Benefits from "./benefits/Benefits.component";
 import Price from "./price/Price.component";
 import Features from "./features/features.component";
 import Faq from "./faq/faq.component";
-import Footer from "../footer/Footer.page";
-import { Navbar } from "../navbar/navbar";
 
 export default function LandingPagePage() {
 	return (
 		<div>
-			<Navbar />
 			<div className="bg-custom-light-grayish-blue2">
 				<section id="home">
 					<Hero />
@@ -33,10 +30,6 @@ export default function LandingPagePage() {
 					<Price />
 				</section>
 				<br />
-				<section id="">
-					<Footer />
-				</section>
-				
 			</div>
 		</div>
 	);
