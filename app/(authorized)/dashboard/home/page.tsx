1687b35d263baf5ab4ee33adbe7e6227f05c0597
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-11-27 18:57:49 +0200

    Adjusting css Grid settings

--- app/(authorized)/dashboard/home/page.tsx
+++ app/(authorized)/dashboard/home/page.tsx
@@ -1,9 +1,14 @@
-import Campaigns from '@/components/authorized/home/Campaigns.component'
+import Campaigns from "@/components/authorized/home/Campaigns.component";
 
 export default function Page() {
   return (
-    <div className="bg-blue-200 h-[3000px]">
-      <Campaigns />
+    <div className="w-full">
+      <div className="px-[1.5em] h-[auto] max-w-[1500px] min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3 content-center w-full">
+        <Campaigns />
+        <Campaigns />
+        <Campaigns />
+        <Campaigns />
+      </div>
     </div>
-  )
+  );
 }
--- components/authorized/home/Campaigns.component.tsx
+++ components/authorized/home/Campaigns.component.tsx
@@ -1,88 +1,95 @@
-'use client';
-import DataViewHome from '@/components/ui/data-view';
-import { classNames } from 'primereact/utils';
-import { Button } from 'primereact/button';
+"use client";
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Divider from "@mui/material/ListItemAvatar";
+import Checkbox from "@mui/material/Checkbox";
+import Avatar from "@mui/material/Avatar";
 
-function Header() {
-  return (
-    <h3>Header</h3>
-   )
-}
+export default function CheckboxListSecondary() {
+  const [checked, setChecked] = React.useState([1]);
 
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
 
-export default function Campaigns() {
-  const data = [
-    {
-      id: 'f230fh0g3',
-      name: 'Tech',
-      amount: 15000,
-      logo: 'pi-microchip'
-    },
-    {
-      id: 'asglkj21235',
-      name: 'Commerce',
-      amount: 11612,
-      logo: 'pi-shopping-cart',
-    },
-    {
-      id: '1351asdfgjl',
-      name: 'Business',
-      amount: 581235,
-      logo: 'pi-briefcase',
-    },
-    {
-      id: 'jasg61234',
-      name: 'Fashion',
-      amount: 622000,
-      logo: 'pi-apple',
-    },
-    {
-      id: 'jg235nba51',
-      name: 'Entertainment',
-      amount: 90012,
-      logo: 'pi-play',
-    },
-  ]
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
 
-  const itemTemplate = (product, index) => {
-    return (
-      <div className="col-12" key={product.id}>
-        <div className={classNames('flex flex-column xl:flex-row xl:align-items-start p-4 gap-4', { 'border-top-1 surface-border': index !== 0 })}>
-
-            {/* <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`} alt={product.name} /> */}
-
-            <div className="flex flex-column sm:flex-row justify-between align-center xl:align-items-start flex-1 gap-4 ">
-                <div className="flex flex-column align-items-center sm:align-items-start gap-3 ">
-                    <div className="flex flex-col justify-center"><span className={`pi ${product.logo} text-align-bottom`}></span></div>
-                    <div className="text-2xl font-bold text-900">{product.name}</div>
-                    <div className="flex align-items-center gap-3">
-                        <span className="flex align-items-center gap-2">
-                            <span className="font-semibold">{product.description}</span>
-                        </span>
-                    </div>
-                </div>
-                <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2 ">
-                    <span className="text-2xl font-semibold">${product.amount}</span>
-                </div>
-            </div>
-          </div>
-      </div>
-    );
+    setChecked(newChecked);
   };
 
-  const listTemplate = (items) => {
-    let list = items.map((product, index) => {
-          return itemTemplate(product, index);
-      });
-
-    return <div className="grid grid-nogutter">{list}</div>;
-  }
+  const data = [
+    {
+      id: "f230fh0g3",
+      name: "Tech",
+      amount: 15000,
+      logo: "pi-microchip",
+    },
+    {
+      id: "asglkj21235",
+      name: "Commerce",
+      amount: 11612,
+      logo: "pi-shopping-cart",
+    },
+    {
+      id: "1351asdfgjl",
+      name: "Business",
+      amount: 581235,
+      logo: "pi-briefcase",
+    },
+    {
+      id: "jasg61234",
+      name: "Fashion",
+      amount: 622000,
+      logo: "pi-apple",
+    },
+    {
+      id: "jg235nba51",
+      name: "Entertainment",
+      amount: 90012,
+      logo: "pi-play",
+    },
+  ];
 
   return (
-    <section className="">
-      <div>
-        <DataViewHome value={data} listTemplate={listTemplate} className={`border-2`} layout='list' header={Header()}  />
-      </div>
-    </section>
-  )
+    <List
+      sx={{}}
+      subheader={
+        <h3 className="pl-[1em] text-2xl bg-custom-very-soft-blue ">
+          Component
+        </h3>
+      }
+      className="w-full max-h-[25vh] border-2 overflow-y-auto bg-custom-light-grayish-blue2 rounded-md"
+    >
+      {data.map((value) => {
+        const labelId = `checkbox-list-secondary-label-${value}`;
+        return (
+          <div key={value.id}>
+            <ListItem
+              key={value.id}
+              secondaryAction={<ListItemText>${value.amount}</ListItemText>}
+              disablePadding
+            >
+              <ListItemButton className="">
+                <ListItemAvatar>
+                  <div className="flex flex-row justify-center">
+                    <span className={`pi ${value.logo} text-xl `}></span>
+                  </div>
+                </ListItemAvatar>
+                <ListItemText id={labelId} primary={`${value.name}`} />
+              </ListItemButton>
+            </ListItem>
+            <Divider />
+          </div>
+        );
+      })}
+    </List>
+  );
 }
