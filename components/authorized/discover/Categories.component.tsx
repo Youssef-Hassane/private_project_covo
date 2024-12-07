4198904aeda9dac9d2999d0167f5e3fd3d95cd92
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-12-07 07:51:17 +0200

    added click-ability to component buttons

--- components/authorized/discover/Categories.component.tsx
+++ components/authorized/discover/Categories.component.tsx
@@ -1,3 +1,115 @@
+'use client';
+
+// import * as React from "react";
+// import { Button } from "@/components/ui/button";
+// import { Input } from "@/components/ui/input";
+// import {
+//   Feather,
+//   Cpu,
+//   Sparkles,
+//   Volleyball,
+//   ShoppingBag,
+//   Pizza,
+//   Cross,
+//   PaintRoller,
+//   Rows3,
+// } from "lucide-react";
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card";
+
+// export default function Categories() {
+//   const [isSelected, setSelected] = React.useState(false);
+
+//   const categoriesArray = [
+//     {
+//       name: "Food and Beverage",
+//       icon: <Pizza />,
+//       color: "#98ACCF",
+//     },
+//     {
+//       name: "E-commerce",
+//       icon: <ShoppingBag />,
+//       color: "#CAD4E8",
+//     },
+//     {
+//       name: "Sports and Athletics",
+//       icon: <Volleyball />,
+//       color: "#398882",
+//     },
+//     {
+//       name: "Beauty and Cosmetics",
+//       icon: <Sparkles />,
+//       color: "#EDCBD3",
+//     },
+//     {
+//       name: "Technology",
+//       icon: <Cpu />,
+//       color: "#00C6BA",
+//     },
+//     {
+//       name: "Fashion",
+//       icon: <Feather />,
+//       color: "#0095FA",
+//     },
+//     {
+//       name: "Health and Wellness",
+//       icon: <Cross />,
+//       color: "#EDCBD3",
+//     },
+//     {
+//       name: "Home and Decor",
+//       icon: <PaintRoller />,
+//       color: "#9CA6B9",
+//     },
+//     {
+//       name: "All categories",
+//       icon: <Rows3 />,
+//       color: "#9CA6B9",
+//     },
+//   ];
+
+//   const handleClickEvent = (event) => {
+//     event.preventDefault();
+//     console.log(event.target);
+//     setSelected(!isSelected);
+//   }
+
+//   return (
+//     <div className="flex flex-col gap-1 pt-[1em]">
+//       <h3 className="font-extrabold text-xl">Categories</h3>
+//       <div className="flex items-center flex-wrap gap-2 ">
+//         {
+//           categoriesArray.map((item) => {
+//             return (
+//               <button key={item.name} onClick={handleClickEvent} >
+//                 <Card className={ `flex flex-row p-2 items-center ${isSelected ? "bg-blue-200 border-blue-500" : "bg-[hsl(var(--sidebar-border))]"} duration-300 `} >
+//                   <CardHeader
+//                   className= "flex flex-col justify-center p-2 items-center h-10 w-10 border-2 rounded-full "
+//                   style={{ backgroundColor: item.color }} >
+//                     <CardDescription className="text-white " >{item.icon}</CardDescription>
+//                   </CardHeader>
+//                   <CardContent className="flex flex-col justify-center py-[0.5em] px-[1em] ">
+//                     <CardTitle className="">{item.name}</CardTitle>
+//                     <CardDescription className="">
+//                       Something
+//                     </CardDescription>
+//                   </CardContent>
+//                 </Card>
+//               </button>
+//             );})
+//         }
+//       </div>
+//     </div>
+//   );
+// }
+
+import * as React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -20,74 +132,111 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+const categoriesArray = [
+  {
+    name: "Food and Beverage",
+    icon: <Pizza />,
+    color: "#98ACCF",
+    selected: false,
+  },
+  {
+    name: "E-commerce",
+    icon: <ShoppingBag />,
+    color: "#CAD4E8",
+    selected: false,
+  },
+  {
+    name: "Sports and Athletics",
+    icon: <Volleyball />,
+    color: "#398882",
+    selected: false,
+  },
+  {
+    name: "Beauty and Cosmetics",
+    icon: <Sparkles />,
+    color: "#EDCBD3",
+    selected: false,
+  },
+  {
+    name: "Technology",
+    icon: <Cpu />,
+    color: "#00C6BA",
+    selected: false,
+  },
+  {
+    name: "Fashion",
+    icon: <Feather />,
+    color: "#0095FA",
+    selected: false,
+  },
+  {
+    name: "Health and Wellness",
+    icon: <Cross />,
+    color: "#EDCBD3",
+    selected: false,
+  },
+  {
+    name: "Home and Decor",
+    icon: <PaintRoller />,
+    color: "#9CA6B9",
+    selected: false,
+  },
+  {
+    name: "All categories",
+    icon: <Rows3 />,
+    color: "#9CA6B9",
+    selected: false,
+  },
+];
+
+
 export default function Categories() {
-  const categoriesArray = [
-    {
-      name: "Food and Beverage",
-      icon: <Pizza />,
-      color: "#98ACCF",
-    },
-    {
-      name: "E-commerce",
-      icon: <ShoppingBag />,
-      color: "#CAD4E8",
-    },
-    {
-      name: "Sports and Athletics",
-      icon: <Volleyball />,
-      color: "#398882",
-    },
-    {
-      name: "Beauty and Cosmetics",
-      icon: <Sparkles />,
-      color: "#EDCBD3",
-    },
-    {
-      name: "Technology",
-      icon: <Cpu />,
-      color: "#00C6BA",
-    },
-    {
-      name: "Fashion",
-      icon: <Feather />,
-      color: "#0095FA",
-    },
-    {
-      name: "Health and Wellness",
-      icon: <Cross />,
-      color: "#EDCBD3",
-    },
-    {
-      name: "Home and Decor",
-      icon: <PaintRoller />,
-      color: "#9CA6B9",
-    },
-    {
-      name: "All categories",
-      icon: <Rows3 />,
-      color: "#9CA6B9",
-    },
-  ];
+  const [selectedCategories, setSelectedCategories] = React.useState(categoriesArray);
+
+  const handleClickEvent = (item, event) => {
+    setSelectedCategories(
+      selectedCategories.map((category) =>
+        category.name === item.name
+          ?
+            category.selected
+              ? { ...category, selected: false }
+              : { ...category, selected: true }
+          : { ...category }
+      )
+    );
+  };
 
   return (
-    <div className="flex items-center pt-[1em] flex-wrap gap-2 ">
-      {categoriesArray.map((item) => {
-        return (
-          <Card key={item.name} className="flex flex-row p-2 " >
-            <CardHeader
-            className= "flex flex-col justify-center p-4 items-center h-[10] w-[10] rounded-full "
-            style={{ backgroundColor: item.color }} >
-              <CardDescription className="text-white" >{item.icon}</CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-col justify-center py-[0.5em] px-[1em] ">
-              <CardTitle className="">{item.name}</CardTitle>
-              <CardDescription className="">
-                Something
-              </CardDescription>
-            </CardContent>
-          </Card>
-        );
-      })}
+    <div className="flex flex-col gap-1 pt-[1em]">
+      <h3 className="font-extrabold text-xl">Categories</h3>
+      <div className="flex items-center flex-wrap gap-2 ">
+        {categoriesArray.map((item) => {
+          const isSelected = selectedCategories.find(
+            (category) => category.name === item.name
+          ).selected;
+
+          return (
+            <button key={item.name} onClick={(event) => handleClickEvent(item, event)}>
+              <Card
+                className={`flex flex-row p-2 items-center ${
+                  isSelected ? "bg-blue-200 border-blue-500" : "bg-[hsl(var(--sidebar-border))]"
+                } duration-300`}
+              >
+                <CardHeader
+                  className="flex flex-col justify-center p-2 items-center h-10 w-10 border-2 rounded-full"
+                  style={{ backgroundColor: item.color }}
+                >
+                  <CardDescription className="text-white">{item.icon}</CardDescription>
+                </CardHeader>
+                <CardContent className="flex flex-col justify-center py-[0.5em] px-[1em]">
+                  <CardTitle className="">{item.name}</CardTitle>
+                  <CardDescription className="">Something</CardDescription>
+                </CardContent>
+              </Card>
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
